44b2bbdb6b577671217005f5a90ef62f5c415eb5
tonix11 <gavrilin0295@gmail.com>
2022-04-21 17:02:49 +0300

    3 practice

---
+++ practice3/IntentApp/src/main/java/ru/mirea/gavrilin/practice3/MainActivity.java
@@ -0,0 +1,29 @@
+package ru.mirea.gavrilin.practice3;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void onSecondActClick(View view) {
+        Intent intent = new Intent(this, MainActivity2.class);
+
+        long dateInMillis = System.currentTimeMillis();
+        String format = "yyyy-MM-dd HH:mm:ss";
+        final SimpleDateFormat sdf = new SimpleDateFormat(format);
+        String dateString = sdf.format(new Date(dateInMillis));
+        intent.putExtra("date", dateString);
+        startActivity(intent);
+    }
+}
